3a1730e4aafab3012c39ab68417052050b64ed65
Prakriti Kayastha <prakayastha314@gmail.com>
2025-07-07 10:26:35 +0100

    clean

--- phonons/n2_I4_mmm_DFT/makefolders.py
+++
@@ -1,8 +0,0 @@
-import os
-
-for i in range(1,23):
-        os.mkdir('phonopy-FHI-aims-displacement-%03d'%i)
-        os.chdir('phonopy-FHI-aims-displacement-%03d'%i)
-        os.system('mv ../geometry.in-%03d geometry.in '%i)
-        os.system('cp ../control.in .')
-        os.chdir('../')
